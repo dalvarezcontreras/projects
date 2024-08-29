1fd5f12ff5a281a85e2000e79fe8cbe931c57b0a
Diego Alvarez <alfaman360@gmail.com>
2024-08-29 12:10:12 -0700

    w commit

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Inner } from "./styles/jumbotron";
+
+export default function Jumbotron({}) {//this function operates the the big page upon opening that gives option to either sign in or login 
+    return (
+        <Inner>
+            <p>Hello!</p>
+        </Inner>
+    )
+}
